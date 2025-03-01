dc18773d7472f25da6da60b93442972bde2d2034
kelaner <lanercifang@outlook.com>
2025-03-01 15:14:52 +0800

    update use-chart.ts

--- src/components/chart/use-chart.ts
+++ src/components/chart/use-chart.ts
@@ -97,11 +97,11 @@ export function useChart(options?: ChartOptions): ChartOptions {
       ...options?.states,
       hover: {
         ...options?.states?.hover,
-        filter: { type: 'darken', value: 0.88, ...options?.states?.hover?.filter },
+        filter: { type: 'darken', value: 0.88, ...options?.states?.hover?.filter } as any,
       },
       active: {
         ...options?.states?.active,
-        filter: { type: 'darken', value: 0.88, ...options?.states?.active?.filter },
+        filter: { type: 'darken', value: 0.88, ...options?.states?.active?.filter } as any,
       },
     },
 
@@ -207,7 +207,7 @@ export function useChart(options?: ChartOptions): ChartOptions {
       fontWeight: 500,
       fontSize: '13px',
       horizontalAlign: 'right',
-      markers: { radius: 12 },
+      markers: { radius: 12 } as any,
       labels: {
         colors: theme.vars.palette.text.primary,
       },
